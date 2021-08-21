1594173390ebca36bd2bb229551a700ca90bdc92
Pham Long Huan <huanvip455@gmail.com>
2021-08-21 16:34:45 +0700

    fix auth 2

--- server/src/midlewares/authMidleware.js
+++ server/src/midlewares/authMidleware.js
@@ -1,30 +1,30 @@
 const verifyIdToken = require('../cores/authVerify');
 
 async function authMidleware(req, res, next) {
-    // const authHeader = req.header('Authorization');
+    const authHeader = req.header('Authorization');
 
-    // const token = authHeader && authHeader.split(' ')[1];
+    const token = authHeader && authHeader.split(' ')[1];
 
 
-    // // Khong co Token
-    // if (!token)
-    //     return res
-    //         .status(401)
-    //         .json({ success: false, message: "Didn't login" });
+    // Khong co Token
+    if (!token)
+        return res
+            .status(401)
+            .json({ success: false, message: "Didn't login" });
 
-    // // Co token
-    // try {
-    //     const decodedToken = await verifyIdToken(token);
-    //     req.userInfo = decodedToken;
-    //     next();
+    // Co token
+    try {
+        const decodedToken = await verifyIdToken(token);
+        req.userInfo = decodedToken;
+        next();
 
-    // } catch (err) {
-    //     console.log(err);
-    //     return res
-    //         .status(500)
-    //         .json({ success: false, message: 'Invalid token' });
-    // }
-    next();
+    } catch (err) {
+        console.log(err);
+        return res
+            .status(500)
+            .json({ success: false, message: 'Invalid token' });
+    }
+    // next();
 }
 
 module.exports = authMidleware;
